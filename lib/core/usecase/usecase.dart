105f9c24da9874bd21786c0ca376bef516de4d50
Antoniiocg7 <antoniocanizares8820@gmail.com>
2024-05-01 17:21:07 +0200

    Sign Up Usecase

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,7 @@
+
+import 'package:blog_app/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class Usecase<SuccesType, Params> {
+  Future<Either<Failure, SuccesType>> call(Params params);
+}
